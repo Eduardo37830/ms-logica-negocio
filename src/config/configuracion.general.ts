c58f6d411b0d0f91a2a0f26ac8813bd450a861ea
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-04-03 23:28:42 -0500

    Carga y descarga de archivos en logica de negocio

---
+++ src/config/configuracion.general.ts
@@ -0,0 +1,11 @@
+export namespace ConfiguracionGeneral {
+  export const carpetaArchivosProductos: string = '../../archivos/productos';
+  export const carpetaArchivosClientes: string = '../../archivos/clientes';
+  export const campoDeProducto: string = 'file';
+  export const extensionesImagenes: string[] = [
+    '.JPG',
+    '.JPEG',
+    '.PNG',
+    '.GIF',
+  ];
+}
